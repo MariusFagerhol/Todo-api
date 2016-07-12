84e52bb053a867c1774c612ab9c27fb91bb847ee
MariusFagerhol <marius.fagerhol@sharebox.no>
2016-07-12 14:51:32 +0200

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,33 @@
+/* var person = {
+    name: "marius",
+    age: 21
+};
+
+function updatePerson(obj) {
+  //  obj = {
+//        name: "marius",
+//        age: 24
+  //  };
+    obj.age = 24;
+}
+*/
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var grades = [15, 88];
+
+function addGrades(grades) {
+    var TheNumber = 54;
+    grades.push(TheNumber);
+    debugger;
+  //  for (var i = 0; i < 10; i++) {
+    //    TheNumber++;
+    //    grades.push(TheNumber);
+//    }
+}
+addGrades(grades);
+console.log(grades);
+
